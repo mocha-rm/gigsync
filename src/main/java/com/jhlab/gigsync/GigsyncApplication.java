e0e0596df2e1fbfe3bf39db66c8503269b61c750
Park JaeHong <blogerhong@naver.com>
2025-03-28 22:29:00 +0900

    init commit

---
+++ src/main/java/com/jhlab/gigsync/GigsyncApplication.java
@@ -0,0 +1,13 @@
+package com.jhlab.gigsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhlab/gigsync/GigsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhlab.gigsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
