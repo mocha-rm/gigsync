903937c8ba793f8355805589f75de69b25c6abe4
Park JaeHong <blogerhong@naver.com>
2025-05-08 17:28:19 +0900

    add : 깃허브액션 yml파일 추가 및 docker-compose.yml 수정

--- src/test/java/com/jhlab/gigsync/GigsyncApplicationTests.java
+++ src/test/java/com/jhlab/gigsync/GigsyncApplicationTests.java
@@ -1,13 +1,12 @@
 package com.jhlab.gigsync;
 
+import com.jhlab.gigsync.global.common.config.S3Config;
+import com.jhlab.gigsync.global.common.service.FileService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class GigsyncApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
