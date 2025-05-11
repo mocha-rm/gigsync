c5666cf30bfcffc9b77dda699291fa64754a3943
Park JaeHong <blogerhong@naver.com>
2025-05-11 10:56:47 +0900

    fix : frontend domain edit

--- src/main/java/com/jhlab/gigsync/global/security/config/SecurityConfig.java
+++ src/main/java/com/jhlab/gigsync/global/security/config/SecurityConfig.java
@@ -82,7 +82,7 @@ public class SecurityConfig {
             var config = new org.springframework.web.cors.CorsConfiguration();
             config.setAllowedOrigins(List.of
                     (
-                            "http://ec2-3-39-233-166.ap-northeast-2.compute.amazonaws.com",
+                            "http://ec2-54-180-83-28.ap-northeast-2.compute.amazonaws.com",
                             "http://localhost:5173"
                     )
             );
